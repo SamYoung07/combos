1cbc796581b0b93cabc63b81ea30ecedfe8ccd83
SamYoung07 <samuelm.young07@gmail.com>
2025-05-23 22:51:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ st.title("Combo Generator")
 st.write("Randomly Generate a Boxing Combo")
 
 options = [3,4,5]
-jab = Image.open("images\Jab.jpg")
-cross = Image.open("images\Cross.jpg")
-l_hook = Image.open("images\l-hook.jpg")
-r_hook = Image.open("images\d-hook.jpg")#r dont work :(
-l_cut = Image.open("images\l-uppercut.jpg")
-r_cut = Image.open("images\d-uppercut.jpg")#r dont work :(
-slip = Image.open("images\slip.jpg")
+jab = Image.open(r"images\Jab.jpg")
+cross = Image.open(r"images\Cross.jpg")
+l_hook = Image.open(r"images\l-hook.jpg")
+r_hook = Image.open(r"images\d-hook.jpg")#r dont work :(
+l_cut = Image.open(r"images\l-uppercut.jpg")
+r_cut = Image.open(r"images\d-uppercut.jpg")#r dont work :(
+slip = Image.open(r"images\slip.jpg")
 
 moves = [jab, cross, l_hook, r_hook, l_cut, r_cut, slip]
 
